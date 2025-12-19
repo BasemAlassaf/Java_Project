bf01280848af553f948c7180f7d11b55c7bd03e7
Stevan <almhannastevan@gmail.com>
2025-12-19 13:18:01 +0300

    Add some methods

--- src/AddNewProductLineGUI.java
+++ src/AddNewProductLineGUI.java
@@ -1,3 +1,9 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -5,12 +11,6 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 public class AddNewProductLineGUI extends JFrame implements ActionListener,MouseListener{
 
     JTextField productLineName, productLineNumber;
